3a24795ba0e9af3ee3d9c1a74a98045d0c9bb866
Aries.Wang <aries.wang@bybit.com>
2023-03-23 14:47:03 +0800

    erc721 test demo

--- test/ERC721.js
+++ test/ERC721.js
@@ -4,122 +4,57 @@ const {
 } = require("@nomicfoundation/hardhat-network-helpers");
 const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
+const hre = require("hardhat");
 
-describe("Lock", function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshot in every test.
-  async function deployOneYearLockFixture() {
-    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    const ONE_GWEI = 1_000_000_000;
+describe("Erc721", function () {
 
-    const lockedAmount = ONE_GWEI;
-    const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
+  async function deployErc721() {
+   const [owner, user1, user2] = await hre.ethers.getSigners();
+    const Er721 = await hre.ethers.getContractFactory("ERC721Demo");
+    const  erc721 = await Er721.deploy("ERC721-TEST","ERC721T", 1);
+    console.log("erc721 token: ", erc721.address);
 
-    // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount] = await ethers.getSigners();
-
-    const Lock = await ethers.getContractFactory("Lock");
-    const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-    return { lock, unlockTime, lockedAmount, owner, otherAccount };
+    return { erc721, owner, user1, user2 };
   }
 
   describe("Deployment", function () {
-    it("Should set the right unlockTime", async function () {
-      const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
-
-      expect(await lock.unlockTime()).to.equal(unlockTime);
-    });
-
     it("Should set the right owner", async function () {
-      const { lock, owner } = await loadFixture(deployOneYearLockFixture);
+      const { erc721, owner,user1, user2 } = await loadFixture(deployErc721);
 
-      expect(await lock.owner()).to.equal(owner.address);
+      expect(await erc721.owner()).to.equal(owner.address);
     });
 
-    it("Should receive and store the funds to lock", async function () {
-      const { lock, lockedAmount } = await loadFixture(
-        deployOneYearLockFixture
-      );
+    it("Should send token 1 to owner", async function () {
+      const { erc721, owner,user1, user2 } = await loadFixture(deployErc721);
 
-      expect(await ethers.provider.getBalance(lock.address)).to.equal(
-        lockedAmount
-      );
-    });
-
-    it("Should fail if the unlockTime is not in the future", async function () {
-      // We don't use the fixture here because we want a different deployment
-      const latestTime = await time.latest();
-      const Lock = await ethers.getContractFactory("Lock");
-      await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-        "Unlock time should be in the future"
-      );
+      expect(await erc721.ownerOf(1)).to.equal(owner.address);
     });
   });
 
-  describe("Withdrawals", function () {
-    describe("Validations", function () {
-      it("Should revert with the right error if called too soon", async function () {
-        const { lock } = await loadFixture(deployOneYearLockFixture);
+  describe("Mint", function () {
+    describe("Mint new token", function () {
+      it("Should revert with wrong owner", async function () {
+        const { erc721, owner,user1, user2 } = await loadFixture(deployErc721);
 
-        await expect(lock.withdraw()).to.be.revertedWith(
-          "You can't withdraw yet"
+        await expect(erc721.connect(user1).mint(user2.address, 2)).to.be.revertedWith(
+          "Ownable: caller is not the owner"
         );
       });
 
-      it("Should revert with the right error if called from another account", async function () {
-        const { lock, unlockTime, otherAccount } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        // We can increase the time in Hardhat Network
-        await time.increaseTo(unlockTime);
-
+      it("Should mint the new nft to user", async function () {
+        const { erc721, owner,user1, user2 } = await loadFixture(deployErc721);
+        await erc721.mint(user2.address, 22);
         // We use lock.connect() to send a transaction from another account
-        await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-          "You aren't the owner"
-        );
-      });
-
-      it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-        const { lock, unlockTime } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        // Transactions are sent using the first signer by default
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).not.to.be.reverted;
+        await expect(await erc721.ownerOf(22)).to.equal(user2.address);
       });
     });
 
     describe("Events", function () {
-      it("Should emit an event on withdrawals", async function () {
-        const { lock, unlockTime, lockedAmount } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw())
-          .to.emit(lock, "Withdrawal")
-          .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-      });
-    });
-
-    describe("Transfers", function () {
-      it("Should transfer the funds to the owner", async function () {
-        const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).to.changeEtherBalances(
-          [owner, lock],
-          [lockedAmount, -lockedAmount]
-        );
+      it("Should emit an event on mint", async function () {
+        const { erc721, owner,user1, user2 } = await loadFixture(deployErc721);
+        await expect(erc721.mint(user2.address, 33))
+          .to.emit(erc721, "Transfer")
+          .withArgs(hre.ethers.constants.AddressZero, user2.address, 33); // We accept any value as `when` arg
       });
     });
   });
